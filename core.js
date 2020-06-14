e1e60aec29117787d16a09b77774114dabfc9504
xiehuaqiang <xiehuaqiang@xiaojubianli.com>
2020-06-14 20:43:27 +0800

    修复接口地址与readme中声明的不一致问题&增加代码注释

--- core.js
+++ core.js
@@ -17,15 +17,20 @@ async function request(url, type) {
 }
 
 async function runDouyin(shareUrl) {
+  // 1.根据分享的视频地址，通过重定向获取整个html信息
   const { data: html } = await request(shareUrl);
+  // 2.截取itemId， dytk 发起二次请求获取uriId
   const itemId = html.match(/(?<=itemId:\s\")\d+(?=\")/g)[0];
   const dytk = html.match(/(?<=dytk:\s\")(.*?)(?=\")/g)[0];
   const long_url = `https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=${itemId}&dytk=${dytk}`;
   const { data: videoJson } = await request(long_url);
+  // 3.最后通过uri参数来调用视频下载接口
   const uriId = videoJson.item_list[0].video.play_addr.uri;
   const desc = videoJson.item_list[0].desc;
   const noWatermarkUrl = `https://aweme.snssdk.com/aweme/v1/play/?video_id=${uriId}&line=0&ratio=540p&media_type=4&vr_type=0&improve_bitrate=0&is_play_url=1&is_support_h265=0&source=PackSourceEnum_PUBLISH`;
   const { data: videoStream } = await request(noWatermarkUrl, 'stream');
+  const { data } = await request(noWatermarkUrl, 'stream');
+  console.log('data: ', data)
   return { videoStream, desc };
 }
 
